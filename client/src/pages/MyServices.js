cb1d5cb7e6e1a68f4b157715e2ba917eac4160f3
Moises Saul Perez Rodriguez <mperez@americas.phoenixcontact.com>
2023-05-29 20:06:42 -0600

    Adding Tailwind functionality

--- client/src/pages/MyServices.js
+++ client/src/pages/MyServices.js
@@ -1,30 +1,28 @@
 import React from "react";
-import { Container, Card, Button, Row, Col } from "react-bootstrap";
-
 import { useQuery, useMutation } from "@apollo/client";
-import { useParams } from "react-router-dom";
-
+import { GET_ME, GET_VENDORS } from "../utils/queries";
+import { SAVE_SERVICE } from "../utils/mutations";
 import Auth from "../utils/auth";
 
-import { GET_CUSTOMER_BY_ID } from "../utils/queries";
-import { UNSAVE_SERVICE } from "../utils/mutations";
+const VendorList = () => {
+  const [saveService, { error }] = useMutation(SAVE_SERVICE);
+  const { loading: vendorsLoading, data: vendorsData } = useQuery(GET_VENDORS);
+  const vendors = vendorsData?.getVendors || [];
 
-const MyServices = () => {
-  const { profileId } = useParams();
-  const [unsaveService] = useMutation(UNSAVE_SERVICE);
-  const { loading, data } = useQuery(GET_CUSTOMER_BY_ID, {
-    variables: { userId: profileId },
-  });
-  // if data isn't here yet, say so
-  if (loading) {
-    return <h2>LOADING...</h2>;
+  let userData = {};
+  const { data: meData } = useQuery(GET_ME, { skip: !Auth.loggedIn() });
+
+  if (Auth.loggedIn()) {
+    userData = meData?.me || {};
   }
 
-  //   console.log(profileId);
+  if (vendorsLoading) {
+    return <p>Loading vendors...</p>;
+  }
 
-  const userData = data.getCustomerById;
+  const handleSaveService = async (event, serviceId) => {
+    event.preventDefault();
 
-  const handleDeleteService = async (serviceId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -32,55 +30,51 @@ const MyServices = () => {
     }
 
     try {
-      const response = await unsaveService({ variables: { serviceId } });
-      console.log(response);
-      window.location.reload();
+      const data = await saveService({
+        variables: { serviceId: serviceId },
+      });
+      console.log(data);
     } catch (err) {
-      console.error(err);
+      console.log(err);
     }
   };
 
   return (
-    <>
-      <div fluid className="text-light bg-dark p-5">
-        <Container>
-          <h1>Viewing saved services!</h1>
-        </Container>
-      </div>
-      <Container>
-        <h2>
-          {userData.savedExperiences.length
-            ? `Viewing ${userData.savedExperiences.length} saved ${
-                userData.savedExperiences.length === 1
-                  ? "experience"
-                  : "experiences"
-              }:`
-            : "You have no saved experiences!"}
-        </h2>
-        <Row>
-          {userData.savedExperiences.map((experience) => {
-            return (
-              <Col md="4">
-                <Card key={experience._id} border="dark">
-                  <Card.Body>
-                    <Card.Title>{experience.name}</Card.Title>
-                    <p className="small">Vendor: {experience.vendor.name}</p>
-                    <Card.Text>{experience.description}</Card.Text>
-                    <Button
-                      className="btn-block btn-danger"
-                      onClick={() => handleDeleteService(experience._id)}
-                    >
-                      Delete this Service!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    </>
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Vendors</h1>
+      {vendors.map((vendor) => (
+        <div key={vendor._id} className="mb-4">
+          <h3 className="text-lg font-semibold">{vendor.name}</h3>
+          <p>Location: {vendor.location}</p>
+          <p>Description: {vendor.description}</p>
+          {userData.usertype === "Customer" && (
+            <>
+              {vendor.services.map((service, index) => (
+                <div key={service._id} className="mt-2">
+                  <p>Service: {service.name}</p>
+                  <button
+                    className="bg-blue-500 text-white px-2 py-1 rounded"
+                    onClick={(event) => handleSaveService(event, service._id)}
+                  >
+                    Save this service
+                  </button>
+                </div>
+              ))}
+            </>
+          )}
+          {userData.usertype !== "Customer" && (
+            <>
+              {vendor.services.map((service, index) => (
+                <div key={service._id} className="mt-2">
+                  <p>Service: {service.name}</p>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
+      ))}
+    </div>
   );
 };
 
-export default MyServices;
+export default VendorList;
